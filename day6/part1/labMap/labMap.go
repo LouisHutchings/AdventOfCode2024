72f6e98d14f8b3da19eac17da94c8c181b179ca9
Louis Hutchings <louishutchings6@gmail.com>
2024-12-09 11:49:45 +0000

    day 6 part 1

---
+++ day6/part1/labMap/labMap.go
@@ -0,0 +1,78 @@
+package labMap
+
+import "errors"
+
+const (
+	Up = iota
+	Right
+	Down
+	Left
+)
+
+type LabMap struct {
+	Walls     []map[int]bool
+	Guard     guard
+	MapBounds []int
+}
+
+type guard struct {
+	x         int
+	y         int
+	direction int
+}
+
+func (lm *LabMap) GetGuardPosition() ([]int, error) {
+	return []int{lm.Guard.x, lm.Guard.y}, nil
+}
+
+func (lm *LabMap) MoveGuard() ([]int, error) {
+	directionToNewPosition := map[int][]int{
+		Up:    {lm.Guard.x, lm.Guard.y - 1},
+		Right: {lm.Guard.x + 1, lm.Guard.y},
+		Down:  {lm.Guard.x, lm.Guard.y + 1},
+		Left:  {lm.Guard.x - 1, lm.Guard.y},
+	}
+	newPosition := directionToNewPosition[lm.Guard.direction]
+	if newPosition[0] > lm.MapBounds[0] || newPosition[1] > lm.MapBounds[1] {
+		return newPosition, errors.New("guard moves out of map")
+	}
+	for lm.isWall(newPosition[0], newPosition[1]) {
+		lm.Guard.turnRight()
+		newPosition = directionToNewPosition[lm.Guard.direction]
+	}
+	lm.Guard.updatePosition(newPosition[0], newPosition[1])
+	return newPosition, nil
+}
+
+func (g *guard) updatePosition(x, y int) {
+	g.x = x
+	g.y = y
+}
+func (g *guard) turnRight() {
+	g.direction = (g.direction + 1) % 4
+}
+func (lm *LabMap) isWall(x int, y int) bool {
+	return lm.Walls[y][x]
+}
+
+func New(wallLocations [][]int, guardLocation []int, rows int) *LabMap {
+	newMap := LabMap{}
+	newMap.initWalls(wallLocations, rows)
+	newMap.initGuard(guardLocation)
+	newMap.MapBounds = []int{rows - 1, rows - 1}
+	return &newMap
+}
+
+func (lm *LabMap) initWalls(wallLocations [][]int, rows int) {
+	lm.Walls = make([]map[int]bool, rows)
+	for i := 0; i < rows; i++ {
+		lm.Walls[i] = make(map[int]bool)
+	}
+	for _, wallLocation := range wallLocations {
+		lm.Walls[wallLocation[0]][wallLocation[1]] = true
+	}
+}
+
+func (lm *LabMap) initGuard(guardLocation []int) {
+	lm.Guard = guard{x: guardLocation[0], y: guardLocation[1], direction: Up}
+}
---
+++ day6/part1/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"day6/part1/labMap"
+	"fmt"
+	"os"
+)
+
+const (
+	FilePath = "/Users/louishut/Documents/adventOfCode/day6input"
+	TestPath = "/Users/louishut/Documents/adventOfCode/day6TestInput"
+)
+
+func main() {
+	input := getInput()
+	lm := labMap.New(getWallLocations(input), getGuardLocation(input), len(input))
+	seenLocations := initSeenLocations(input)
+	for position, err := lm.GetGuardPosition(); err == nil; position, err = lm.MoveGuard() {
+		seenLocations[position[1]][position[0]] = 1
+	}
+	fmt.Println(countSeenLocations(seenLocations))
+}
+
+func countSeenLocations(seenLocations [][]int) int {
+	total := 0
+	for _, row := range seenLocations {
+		for _, location := range row {
+			total += location
+		}
+	}
+	return total
+}
+func getInput() []string {
+	file, err := os.Open(FilePath)
+	check(err)
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var input []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		input = append(input, line)
+	}
+	fmt.Println("rows: ", len(input), "cols: ", len(input[0]))
+	return input
+}
+
+func initSeenLocations(input []string) [][]int {
+	seenLocations := make([][]int, len(input))
+	for i, _ := range seenLocations {
+		seenLocations[i] = make([]int, len(input[0]))
+	}
+	return seenLocations
+}
+
+func getWallLocations(input []string) [][]int {
+	var wallLocations [][]int
+	for i, line := range input {
+		for j, char := range line {
+			if char == '#' {
+				wallLocations = append(wallLocations, []int{i, j})
+			}
+		}
+	}
+	return wallLocations
+}
+
+func getGuardLocation(input []string) []int {
+	for i, line := range input {
+		for j, char := range line {
+			if char == '^' {
+				return []int{j, i}
+			}
+		}
+	}
+	return nil
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
